4bb747058639a5c4e44e4f63afebfca1affaad5e
Wan Shen Lim <wanshen.lim@gmail.com>
2020-10-24 22:12:25 -0400

    Add RETRY functionality to roboczar.

--- noisepage/roboczar.py
+++ noisepage/roboczar.py
@@ -27,6 +27,28 @@ def be_patient(num_seconds):
     time.sleep(num_seconds)
 
 
+def retry_until_good(J, job_name):
+    passing = False
+    while not passing:
+        job = J.get_job(job_name)
+        build = job.get_last_build()
+        print('Jenkins: {} build {}'.format(job_name, build.buildno))
+
+        if build.is_running():
+            print('Jenkins: {} still running, waiting...'.format(job_name))
+            be_patient(300)
+            continue
+
+        if build.is_good():
+            print('Jenkins: {} build {} is good.'.format(job_name, build.buildno))
+            passing = True
+        else:
+            print('Jenkins: {} build {} failed.'.format(job_name, build.buildno))
+            print('Jenkins: trigger build {}.'.format(job_name))
+            J.build_job(job_name)
+            be_patient(10)
+
+
 def merge_pr(J, G, pr):
     repo = G.get_repo('cmu-db/noisepage')
     pull = repo.get_pull(pr['num'])
@@ -52,25 +74,7 @@ def merge_pr(J, G, pr):
         raise Exception('Could not get good merge status for {}. Stop!'.format(pr_name))
 
     # Check if the job is passing on Jenkins.
-    passing = False
-    while not passing:
-        job = J.get_job(pr_name)
-        build = job.get_last_build()
-        print('Jenkins: {} build {}'.format(pr_name, build.buildno))
-
-        if build.is_running():
-            print('Jenkins: Build still running, waiting...'.format(pr_name))
-            be_patient(300)
-            continue
-
-        if build.is_good():
-            print('Jenkins: {} build {} is good.'.format(pr_name, build.buildno))
-            passing = True
-        else:
-            print('Jenkins: {} build {} failed.'.format(pr_name, build.buildno))
-            print('Jenkins: trigger build {}.'.format(pr_name))
-            J.build_job(pr_name)
-            be_patient(10)
+    retry_until_good(J, pr_name)
 
     # Merge the PR on GitHub.
     print('Github: merging {}.'.format(pr_name))
@@ -80,8 +84,8 @@ def merge_pr(J, G, pr):
     print('Github: merged {}.'.format(pr_name))
 
 
-def main():
-    print('The jobs to merge are:')
+def merge_prs():
+    print('The PRs to MERGE are:')
     for i, pr in enumerate(prs, 1):
         print('--------------------------------------------------------------')
         print(i, pr['num'])
@@ -98,6 +102,43 @@ def main():
         print('GitHub: connected.')
         for pr in prs:
             merge_pr(J, G, pr)
+
+
+def retry_prs():
+    jobs = []
+    print('Enter Jenkins jobs to retry (one per line, END to stop).\n')
+
+    done = False
+    while not done:
+        job_name = input('Job: ').strip()
+        if job_name.lower() == 'end':
+            done = True
+        else:
+            jobs.append(job_name)
+
+    print('The jobs to RETRY are:')
+    for i, job in enumerate(jobs, 1):
+        print('--------------------------------------------------------------')
+        print(i, job)
+        print('--------------------------------------------------------------')
+        print()
+    should_continue = input('Continue? [y/n] : ')
+    if should_continue.lower() == 'y':
+        print('Continuing.')
+        J = jenkinsapi.jenkins.Jenkins(jurl, username=juser, password=jpass)
+        print('Jenkins: connected as {}.'.format(juser))
+        for job in jobs:
+            retry_until_good(J, job)
+
+
+def main():
+    option = input('M for merge, R for retry. [M/R] : ')
+    if option.lower() == 'm':
+        merge_prs()
+    elif option.lower() == 'r':
+        retry_prs()
+    else:
+        print('Unknown option: {}'.format(option))
 
 
 if __name__ == '__main__':
